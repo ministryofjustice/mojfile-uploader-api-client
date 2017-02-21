e8aa6fa58a12391d1ec8a465ce2ec9d3327b02d2
Christian Sutter <chris@doublesignal.com>
2017-02-21 16:34:32 +0000

    Add convenience methods to main module
    Adds the following convenience methods to `MojFileUploaderApiClient`:
    - `add_file`
    - `delete_file`
    - `list_files`
    
    These call their respective action classes, and retrieve the body if the
    request was successful – and raise an appropriate error if it wasn't.
    
    This saves us having to call the classes in client code, and having to
    deal with HTTP responses etc.

--- lib/mojfile_uploader_api_client.rb
+++ lib/mojfile_uploader_api_client.rb
@@ -12,4 +12,31 @@ require 'mojfile_uploader_api_client/list_files'
 module MojFileUploaderApiClient
   class Unavailable < StandardError; end
   class RequestError < StandardError; end
+  class InfectedFileError < StandardError; end
+
+  def self.add_file(params)
+    response = AddFile.new(params).call
+
+    if response.success?
+      response.body
+    elsif response.code.equal?(400)
+      raise InfectedFileError
+    else
+      raise RequestError
+    end
+  end
+
+  def self.delete_file(params)
+    response = DeleteFile.new(params).call
+
+    raise RequestError unless response.success?
+    response.body
+  end
+
+  def self.list_files(params)
+    response = ListFiles.new(params).call
+
+    raise RequestError unless response.success?
+    response.body
+  end
 end
